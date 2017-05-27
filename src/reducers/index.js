3e848ca6f0a4894d13376bd76abb0611538e9fbb
tao liu <werewaswas@live.com>
2017-05-27 05:05:37 +0000

    Done.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,13 @@
-export default function ingredientReducer() {
+export default function ingredientReducer(state = [], action) {
+  switch (action.type) {
+    case 'ADD_INGREDIENT':
+      return [...state, action.payload]
 
+    case 'REMOVE_INGREDIENT':
+      let ingredientIndex = state.indexOf(action.payload)
+      return  [...state.slice(0, ingredientIndex), ...state.slice(ingredientIndex + 1)]
+   }
+
+
+  return state;
 }
