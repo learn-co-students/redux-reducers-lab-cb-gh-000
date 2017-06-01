60bd6d252c2049deb51448b581578a6ea363e883
Edmundo Elizondo <edmundo.geg@hotmail.com>
2017-06-01 09:24:52 +0000

    Done.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,21 @@
-export default function ingredientReducer() {
+export default function ingredientReducer(state = [], action) {
+  switch (action.type) {
+    case 'ADD_INGREDIENT':
+      return [...state, action.payload]
+      break
+    case 'REMOVE_INGREDIENT':
+      // In case that payload specifies Index instead of the ingradient...
+      // let newIngredients = [...state]
+      // newIngredients.splice(action.payload, 1)
+      // return newIngredients
 
+      // Return true to keep the element, false to remove. Keep different ones to the specified.
+      return state.filter(ingredient => {
+        let keep = ingredient !== action.payload
+        return keep
+      })
+      break
+    default:
+      return state
+  }
 }
