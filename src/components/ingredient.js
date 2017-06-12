aeef797699f6054fa5213c9dcc842ad1b97296d3
Robert Wayne <robertjwayne@gmail.com>
2017-06-12 14:44:19 -0400

    Done

--- src/components/ingredient.js
+++ src/components/ingredient.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
-export const Ingredient = (props) => {
+const Ingredient = (props) => {
   return (
-    <li
-      className='row'
-      onClick={props.onIngredientClick}>
+    <li>
+      className="row"
+      onClick={props.onIngredientClick}
       {props.ingredient}
     </li>
-  )
-}
+  );
+};
+
+export default Ingredient;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,12 @@
-export default function ingredientReducer() {
-
+export default function ingredientReducer(state = [], action) {
+  switch (action.type) {
+    case 'ADD_INGREDIENT':
+      const ingredient = action.payload;
+      return [...state, ingredient];
+    case 'REMOVE_INGREDIENT':
+      const index = action.payload;
+      return state.filter(item => item !== index );
+    default:
+      return state;
+  }
 }
