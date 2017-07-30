6f09242f3f11b26d133568c52fa081ce27f7e091
J. Ackerman <github email address>
2017-07-30 15:43:13 -0700

    Done.

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,11 @@
-export default function ingredientReducer() {
-
+export default function ingredientReducer(state = [], action) {
+  switch(action.type){
+    case 'ADD_INGREDIENT':
+        return Object.assign([], state, [...state, action.payload]);
+    case 'REMOVE_INGREDIENT':
+        const ingredients = state.splice(action.payload, 1);
+        return Object.assign([], state, ingredients);
+    default:
+        return state;
+  }
 }
